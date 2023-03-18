0d912d42939c9249d0ff657f06ce305ec851c9b9
Ygor Carvalho <ygorsoares2003@gmail.com>
2023-03-18 00:18:55 -0300

    babysteps

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-#aaaaaaaaaa
+// popup
+function selfHelp() {
+   var popup = document.getElementById("myPopup");
+   popup.classList.toggle("show")
+ }
+
+//popup chat window
+function openForm() {
+   document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+   document.getElementById("myForm").style.display = "none";
+}
