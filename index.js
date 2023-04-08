380e3764bc5926cc32f629ed7db9d20f8dd1a5b0
Ygor Carvalho <ygorsoares2003@gmail.com>
2023-04-07 21:36:26 -0300

    newbranch

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const http = require("http");
-const fs = require("fs");
-require('dotenv').config()
+// const http = require("http");
+// const fs = require("fs");
+// require('dotenv').config()
 
-const PORT = process.env.PORT || 5001
+// const PORT = process.env.PORT || 5001
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/html" });
-  fs.createReadStream("./index.html").pipe(res);
-});
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "content-type": "text/html" });
+//   fs.createReadStream(__dirname+"/public").pipe(res);
+// });
 
-server.listen(PORT, () => {
-console.log(process.env.GPT_KEY)
-  console.log(`Server :${PORT}`);
-});
+// server.listen(PORT, () => {
+// console.log(process.env.GPT_KEY)
+//   console.log(`Server :${PORT}`);
+// });
+const express = require('express');
+  const path = require('path');
+  
+  const PORT = process.env.PORT || 4001;
+
+  const app = express();
+  app.use(express.static(`${__dirname}/public`));
+  app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+  });
+  app.listen(PORT);
